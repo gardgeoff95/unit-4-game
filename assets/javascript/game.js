59c34336831b5f0dc2053e6ae40bbb18778afa1b
Geoff Gard <gardgeoff95@email.arizona.edu>
2019-05-25 17:16:13 -0700

    initial commit

---
+++ assets/javascript/game.js
@@ -0,0 +1,43 @@
+  
+  
+  
+$(document).ready(function(){
+  var redGem = $("#redGem");
+  var purpGem = $("#purpGem");
+  var blueGem = $("#blueGem");
+  var greenGem = $("#greenGem");
+
+
+  var redNumb = 0;
+  var purpNumb = 0;
+  var blueNumb = 0;
+  var greenNumb = 0;
+
+
+
+  for(i = 0 i < )
+
+
+  
+
+
+
+
+    
+    $(redGem).on("mouseover",function(){
+        alert("hello");
+    })
+    $(purpGem).on("click",function(){
+        alert("hello");
+    })
+
+    $(blueGem).on("click",function(){
+        alert("hello");
+    })
+
+    $(greenGem).on("click",function(){
+        alert("hello");
+    })
+
+})  
+
