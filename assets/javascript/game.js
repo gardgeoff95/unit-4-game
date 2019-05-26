6b5995d0344152679cc4e18ab5c75147ed54b622
Geoff Gard <gardgeoff95@email.arizona.edu>
2019-05-25 17:43:47 -0700

    Second commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,43 +1,53 @@
-  
-  
-  
-$(document).ready(function(){
-  var redGem = $("#redGem");
-  var purpGem = $("#purpGem");
-  var blueGem = $("#blueGem");
-  var greenGem = $("#greenGem");
-
-
-  var redNumb = 0;
-  var purpNumb = 0;
-  var blueNumb = 0;
-  var greenNumb = 0;
 
 
 
-  for(i = 0 i < )
+$(document).ready(function () {
+    function getRandomInt(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min)) + min;
+    }
+    var redGem = $("#redGem");
+    var purpGem = $("#purpGem");
+    var blueGem = $("#blueGem");
+    var greenGem = $("#greenGem");
 
 
-  
+
+    var redNumb = getRandomInt(1,13);
+    var purpNumb = getRandomInt(1,13);
+    var blueNumb = getRandomInt(1,13);
+    var greenNumb = getRandomInt(1,13);
+
+    console.log(redNumb);
 
 
 
 
-    
-    $(redGem).on("mouseover",function(){
+
+
+
+
+
+
+
+
+
+    $(redGem).on("mouseover", function () {
+        
+
+    })
+    $(purpGem).on("click", function () {
         alert("hello");
     })
-    $(purpGem).on("click",function(){
+
+    $(blueGem).on("click", function () {
         alert("hello");
     })
 
-    $(blueGem).on("click",function(){
+    $(greenGem).on("click", function () {
         alert("hello");
     })
 
-    $(greenGem).on("click",function(){
-        alert("hello");
-    })
-
-})  
+})
 
